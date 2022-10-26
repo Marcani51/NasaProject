6b5f2df78687e47c5eda251c34fc50942cea5e39
Marcani51 <denta.marcellus@gmail.com>
2022-10-27 01:42:55 +0700

    adding test jest in development

---
+++ server/src/routes/launches/launches.test.js
@@ -0,0 +1,19 @@
+describe('Test GET /launches',()=>{
+  test('it should responce with 200 success',()=>{
+    const responce=200;
+    expect(responce).toBe(200);
+  });
+});
+
+describe('Test Post /launch',()=>{
+  test('it should responce with 200 success', () => { 
+    
+  });
+
+  test('it should catch missing required properties', () => { 
+    
+  });
+  test('it should catch invalid date', () => { 
+    
+  });
+});
